47cee3b9ad2205389bc44bf719eba12dc2dbab36
Meisam Fathi Salmi <meisam_fathi@comp.iust.ac.ir>
2010-09-19 09:25:05 +0430

    Undeeded dava files removed from the repository

--- dava/src/mfs/faultmodels/Abc$proceed$mfs_d_faultmodels_d_FaultModels$around$1.java
+++
@@ -1,8 +0,0 @@
-package mfs.faultmodels;
-
-
-public interface Abc$proceed$mfs_d_faultmodels_d_FaultModels$around$1
-{
-
-    public abstract void abc$proceed$mfs_d_faultmodels_d_FaultModels$around$1(int  i0, int  i1);
-}
